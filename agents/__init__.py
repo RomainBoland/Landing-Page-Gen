827d77d97ccc8e32268a40c96f254267f0efc0a1
root <root@jjiro.localdomain>
2026-01-22 14:12:35 +0100

    oui

--- agents/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import Agent
-from .onboarding import OnboardingAgent
-from .brand import BrandAgent
-from .landing import LandingAgent
-
-__all__ = ["Agent", "OnboardingAgent", "BrandAgent", "LandingAgent"]
--- tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Tests for landing-generator."""
